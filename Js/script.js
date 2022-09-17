0e4e5afbe29a1356f6187ddd0e58dc1c17c04da2
SAI GANESH S <83502872+dude-curious@users.noreply.github.com>
2022-09-17 19:41:05 +0530

    preloader updated

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,9 @@
 // -----------preloader-----------
 window.addEventListener('load',()=>{
     const preloader=document.querySelector(".preloader");
-    preloader.style="transform:scaleY(0)";
+
+        preloader.style="transform:scaleY(0)";
+        
 
     window.setTimeout(()=>{
     preloader.style.display='none';
